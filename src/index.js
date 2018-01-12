4909a30e2e14563adfc5172cbdf45ac5805cdfd2
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-01-11 20:22:10 -0800

    added posts_index component

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from "./components/app";
 import reducers from "./reducers";
+import PostsIndex from './components/posts_index';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
@@ -13,8 +13,6 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/posts/new" component={PostsNew} />
-        <Route path="/posts/:id" component={PostsShow} />
         <Route path="/" component={PostsIndex} />
       </div>
     </BrowserRouter>
