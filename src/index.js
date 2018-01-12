1958e54cf7e67c25bc70031570fc5334c764b63d
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-01-11 19:18:21 -0800

    added routes in index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,15 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import App from "./components/app";
 import reducers from "./reducers";
 
-
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/" component={} />
-        <Route path="/" component={} />
-        <Route path="/" component={App} />
+        <Route path="/posts/new" component={PostsNew} />
+        <Route path="/posts/:id" component={PostsShow} />
+        <Route path="/" component={PostsIndex} />
       </div>
     </BrowserRouter>
   </Provider>,
