b889af2df28d6b3e4e3e7317b0868e9e8543f256
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-01-20 15:03:38 -0800

    added ternary op to className for highlighted form fields and error text

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,40 +3,37 @@ import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   renderField(field) {
-    return (
-      <div className="form-group">
+
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ""}`;
+
+    return <div className={className}>
         <label>{field.label}</label>
-        <input 
-          className="form-control"
-          type="text"
-          {...field.input}
-        />
-      </div>
-    );
+        <input className="form-control" type="text" {...field.input} />
+        
+        <div className="text-help">
+          {touched ? error : ""}
+        </div>
+      </div>;
+  }
+  
+  onSubmit(values) {
+    console.log(values);
   }
 
   render() {
-    return (
-      <div>
-        <form>
-          <Field
-            label="Title For Post"
-            name="title"
-            component={this.renderField}
-          />
-          <Field 
-            label="Categories"
-            name="categories"
-            component={this.renderField}
-          />
-          <Field
-            label="Post Content"
-            name="content"
-            component={this.renderField}
-          />
+    const { handleSubmit } = this.props;
+
+    return <div>
+        <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+          <Field label="Title For Post" name="title" component={this.renderField} />
+          <Field label="Categories" name="categories" component={this.renderField} />
+          <Field label="Post Content" name="content" component={this.renderField} />
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
         </form>
-      </div>
-    );
+      </div>;
   }
 }
 
@@ -44,7 +41,7 @@ function validate(values) {
   const errors = {};
 
   if (!values.title) {
-    errors.title = "Enter a title!";
+    errors.title = "Enter a title";
   }
   if (!values.categories) {
     errors.categories = "Enter some categories";
