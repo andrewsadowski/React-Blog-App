5fd6894a62be5e2fd0b4e2706b2fe39482f87893
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-01-21 12:01:32 -0800

    added delete post support

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,20 +1,36 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions';
+import { Link } from 'react-router-dom';
+import { fetchPost, deletePost } from '../actions';
 
 class PostsShow extends Component {
   componentDidMount() {
     const { id } = this.props.match.params;
     this.props.fetchPost(id);
   }
+  
+  onDeleteClick() {
+    const { id } = this.props.match.params;
+    this.props.deletePost(id, () => {
+      this.props.history.push('/');
+    });
+  }
+
   render() {
     const { post } = this.props;
 
     if (!post) {
-      return <div></div>
+      return <div />;
     }
     return (
       <div>
+        <Link to="/" className="btn btn-primary">Back To Index</Link>
+        <button
+          className="btn btn-danger pull-xs-right"
+          onClick={this.onDeleteClick.bind(this)}
+        >
+          Delete Post
+        </button>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
@@ -25,6 +41,6 @@ class PostsShow extends Component {
 
 function mapStateToProps({ posts }, ownProps) {
   return { post: posts[ownProps.match.params.id] };
-};
+}
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
