1cb7edc6205ef3f99ad20606d9a93663d80ee678
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-01-20 16:38:07 -0800

    added support for fetching an individual post: FETCH_POST

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,10 +1,15 @@
-import { FETCH_POSTS, CREATE_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
 import _ from 'lodash';
 
 export default function(state = {}, action) {
   switch (action.type) {
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
+    case FETCH_POST:
+      // const post = actions.payload.data;
+      // const newState = { ...state };
+      // newState[post.id] = post;
+      return { ...state, [action.payload.data.id]: action.payload.data }
     default: 
       return state;
   }
